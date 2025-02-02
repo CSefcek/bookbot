10f4d39ce70006e4fc62d266771800c21baf0521
CSefcek <carlosefcek@gmail.com>
2025-02-03 00:37:06 +0100

    Add report alphabetical characters

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(count_words(file_contents))
-        print(count_characters(file_contents))
+        print("--- Begin report of books/frankenstein.txt ---")
+        count_words(file_contents)
+        dict = count_characters(file_contents)
+        list_of_characters_dict = [{"name": char, "num": count} for char, count in dict.items()]
+        list_of_characters_dict.sort(reverse=True, key=sort_on)
+        
+        for char_dict in list_of_characters_dict:
+            print(f"The '{char_dict['name']}' character was found {char_dict['num']} times")
+        print("--- End report ---")
+        
 
 
 def count_words(text):
-    return len(text.split())
+    count = len(text.split())
+    print(f"{count} words found in the document")
 
 
 
@@ -15,11 +24,22 @@ def count_characters(text):
     occurrences = {}
         
     for char in lowered_text:
-        if char not in occurrences:
-            occurrences[char] = 1
-        else:
-            occurrences[char] += 1
+        if char.isalpha():
+            if char not in occurrences:
+                occurrences[char] = 1
+            else:
+                occurrences[char] += 1
     return occurrences
 
 
+def sort_on(dict):
+    return dict["num"]
+
+
+   
+
+
 main()
+
+
+
